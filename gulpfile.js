8bd01c7148d41e6c62a81c0cb43e49d7a6c6ade4
tomita-koki <pf9555afd752se05se@softbank.ne.jp>
2025-05-11 15:26:53 +0900

    画像、レイアウトモジュールの修正、gulpの調整

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,44 @@
-const gulp = require("gulp");
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 
 // Sassのコンパイルタスク
-gulp.task('cssSass', function () {
-    return gulp.src('_dev/scss/**/*.scss')
+function cssSass() {
+    return src('_dev/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.stream());  // 変更後にブラウザをリロードせずスタイルのみ反映
-});
+        .pipe(dest('src/css'))
+        .pipe(browserSync.stream());
+}
 
 // ブラウザシンクの初期化タスク
-gulp.task("browserSyncTask", function () {
+function browserSyncTask(done) {
     browserSync.init({
         server: {
             baseDir: "src",
         },
+        notify: false,
+        open: true,
+        reloadOnRestart: true,
+        ghostMode: false,
     });
-});
+    done();
+}
 
 // ウォッチタスクの設定
-gulp.task('watch', function () {
-    gulp.watch('_dev/scss/**/*.scss', gulp.series('cssSass'));  
-    gulp.watch("src/**").on('change', browserSync.reload); // 変更があればリロード
-});
+function watchTask() {
+    // SCSSファイルの監視
+    watch('_dev/scss/**/*.scss', cssSass);
+    
+    // HTMLファイルの監視
+    watch('src/**/*.html').on('change', function (path) {
+        browserSync.reload(path);
+    });
+    
+    // JavaScriptファイルの監視
+    watch('src/**/*.js').on('change', function (path) {
+        browserSync.reload(path);
+    });
+}
 
 // デフォルトタスク
-gulp.task('default', gulp.parallel('cssSass', 'browserSyncTask', 'watch'));
+exports.default = parallel(cssSass, browserSyncTask, watchTask);
